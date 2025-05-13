9e31cb34a7f4ec0365b80900dc669a694776eaef
Aminne629 <aminekx3@gmail.com>
2025-05-13 21:57:15 +0200

    debut
    page du debut pas du tt fini...

---
+++ script.js
@@ -0,0 +1,12 @@
+function toggleMenuGauche() {
+    document.getElementById('menu-gauche').classList.toggle('open');
+  }
+  
+  function toggleMenuDroite() {
+    document.getElementById('menu-droite').classList.toggle('open');
+  }
+  
+  function zoomProject(element) {
+    element.classList.toggle('zoomed');
+  }
+  
